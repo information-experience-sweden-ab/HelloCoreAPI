6165b636d9937adb9b28127a9da539f76b42b43d
Sara Ford <saraford@users.noreply.github.com>
2017-11-29 15:10:36 -0800

    working /hello endpoint

---
+++ HelloCoreAPI/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloCoreAPI.Controllers
+{
+
+    public class HelloController : Controller
+    {
+        [Route("hello")]
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello There!";
+        }
+    }
+}
