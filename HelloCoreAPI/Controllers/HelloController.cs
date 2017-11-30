902688efcfac9c88bcf0a328aef0f8abb17643a6
Sara Ford <saraford@users.noreply.github.com>
2017-11-29 16:24:53 -0800

    securing the API

--- HelloCoreAPI/Controllers/HelloController.cs
+++ HelloCoreAPI/Controllers/HelloController.cs
@@ -4,17 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HelloCoreAPI.Controllers
 {
-
+   [Authorize]
     public class HelloController : Controller
     {
         [Route("hello")]
         [HttpGet]
-        public string Hello()
+        public IActionResult Hello()
         {
-            return "Hello There!";
+
+            var scopes = HttpContext.User.FindFirst("http://schemas.microsoft.com/identity/claims/scope")?.Value;
+            if (!string.IsNullOrEmpty("demo.read") && scopes != null && scopes.Split(' ').Any(s => s.Equals("demo.read")))
+            {
+                return Ok("Hello There");
+            }
+            else
+            {
+                return Unauthorized();
+            }
         }
     }
 }
